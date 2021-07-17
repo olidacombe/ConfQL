084596547714f6b2bfcf4a70e570f82ab513c8d7
Oli Dacombe <odacombe@blockchain.com>
2021-07-17 21:54:13 +0300

    test with a more typy type

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-use async_graphql::Object;
+use async_graphql::{Object, SimpleObject};
+
+#[derive(SimpleObject)]
+struct Hero {
+    name: String,
+    powers: Vec<String>,
+}
 
 struct Query;
 
@@ -7,6 +13,13 @@ impl Query {
     /// Returns the sum of a and b
     async fn add(&self, a: i32, b: i32) -> i32 {
         a + b
+    }
+
+    async fn heroes(&self) -> Vec<Hero> {
+        vec![Hero {
+            name: "Bobby".to_owned(),
+            powers: vec![],
+        }]
     }
 }
 
@@ -20,5 +33,12 @@ mod tests {
         let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
         let res = schema.execute("{ add(a: 10, b: 20) }").await;
         assert_eq!(res.data, value!({"add": 30}));
+    }
+
+    #[actix_rt::test]
+    async fn test_test() {
+        let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+        let res = schema.execute("{ heroes { name } }").await;
+        assert_eq!(res.data, value!({"heroes": [{"name": "Bobby"}]}));
     }
 }
