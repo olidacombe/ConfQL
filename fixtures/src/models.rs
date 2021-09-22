4f3a86f29cd4e73c78fc0db7914f9a8482fd1623
Oli Dacombe <odacombe@blockchain.com>
2021-09-22 23:28:30 +0100

    clean slate

--- fixtures/src/models.rs
+++
@@ -1,9 +0,0 @@
-use async_graphql::SimpleObject;
-use serde::Deserialize;
-
-#[derive(PartialEq, Eq, Ord, PartialOrd, SimpleObject, Deserialize, Debug)]
-pub struct Hero {
-    pub name: String,
-    pub id: u32,
-    pub powers: Vec<String>,
-}
