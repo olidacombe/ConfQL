80079ed909b73b6edb9828f40670493784474283
Oli Dacombe <odacombe@blockchain.com>
2021-09-28 23:40:16 +0100

    some fighty tdd

--- src/data_resolver/mod.rs
+++ src/data_resolver/mod.rs
@@ -1,25 +1,88 @@
+use itertools::Itertools;
+use serde::Deserialize;
+use serde_yaml::Value;
+use std::convert::TryFrom;
 use std::path::{Path, PathBuf};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DataResolverError {
+	#[error("Key `{0}` not found")]
+	KeyNotFound(String),
 	#[error("Data not found")]
 	DataNotFound,
+	#[error(transparent)]
+	YamlError(#[from] serde_yaml::Error),
+	#[error(transparent)]
+	IOError(#[from] std::io::Error),
 }
 
-pub struct DataResolver {}
+fn get_sub_value_at_address<'a>(
+	value: &'a Value,
+	address: &[&str],
+) -> Result<&'a Value, DataResolverError> {
+	use itertools::FoldWhile::{Continue, Done};
+	return address
+		.iter()
+		.fold_while(Ok(value), |acc, i| match acc.unwrap().get(i) {
+			Some(v) => Continue(Ok(v)),
+			_ => Done(Err(DataResolverError::KeyNotFound(i.to_string()))),
+		})
+		.into_inner();
+}
 
-impl DataResolver {}
+fn value_from_file(path: &PathBuf) -> Result<Value, DataResolverError> {
+	let file = std::fs::File::open(&path)?;
+	let value = serde_yaml::from_reader::<_, Value>(file)?;
+	Ok(value)
+}
 
-impl From<&Path> for DataResolver {
-	fn from(path: &Path) -> Self {
-		Self {}
+pub struct DataResolver<'a> {
+	root: &'a Path,
+}
+
+impl<'a> DataResolver<'a> {
+	pub fn get_non_nullable<T>(&self, address: &[&str]) -> Result<T, DataResolverError>
+	where
+		T: for<'de> Deserialize<'de>,
+	{
+		self.get_nullable(address)?
+			.ok_or(DataResolverError::DataNotFound)
+	}
+
+	pub fn get_non_nullable_list<T>(&self, address: &[&str]) -> Result<Vec<T>, DataResolverError>
+	where
+		T: for<'de> Deserialize<'de>,
+	{
+		self.get_nullable_list(address)?
+			.ok_or(DataResolverError::DataNotFound)
+	}
+
+	pub fn get_nullable<T>(&self, address: &[&str]) -> Result<Option<T>, DataResolverError>
+	where
+		T: for<'de> Deserialize<'de>,
+	{
+		let path = self.root.join("index.yml");
+		let value = value_from_file(&path)?;
+		let value = get_sub_value_at_address(&value, address)?;
+		let value = serde_yaml::from_value(value.to_owned())?;
+		Ok(value)
+	}
+
+	pub fn get_nullable_list<T>(
+		&self,
+		address: &[&str],
+	) -> Result<Option<Vec<T>>, DataResolverError>
+	where
+		T: for<'de> Deserialize<'de>,
+	{
+		Err(DataResolverError::DataNotFound)
 	}
 }
 
-impl DataResolver {
-	pub fn get_non_nullable<T>(&self, address: &[&str]) -> Result<T, DataResolverError> {
-		Err(DataResolverError::DataNotFound)
+impl<'a> From<&'a Path> for DataResolver<'a> {
+	fn from(path: &'a Path) -> Self {
+		Self { root: path }
 	}
 }
 
