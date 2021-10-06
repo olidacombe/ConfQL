0be2e54f547686bb3f82476a622e6e00ee6a09dd
Oli Dacombe <odacombe@blockchain.com>
2021-10-07 00:02:37 +0100

    move Merge trait into values

--- src/data_resolver/ng.rs
+++ src/data_resolver/ng.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::iter;
 use std::path::PathBuf;
 
-use super::values::{take_sub_value_at_address, value_from_file};
+use super::values::{take_sub_value_at_address, value_from_file, Merge};
 use super::DataResolverError;
 
 enum Level {
@@ -81,89 +81,6 @@ impl<'a> DataPath<'a> {
 		match &self.level {
 			File => false,
 			Dir => self.address.is_empty(),
-		}
-	}
-}
-
-trait Merge {
-	fn merge(&mut self, mergee: Self) -> Result<&mut Self, DataResolverError>;
-	fn merge_at(&mut self, key: &str, mergee: Self) -> Result<&mut Self, DataResolverError>;
-}
-
-macro_rules! merge_compat_err {
-	($self:expr, $mergee:expr) => {
-		Err(DataResolverError::IncompatibleYamlMerge {
-			dst: $self.clone(),
-			src: $mergee,
-		})
-	};
-}
-
-impl Merge for serde_yaml::Value {
-	fn merge(&mut self, mut mergee: Self) -> Result<&mut Self, DataResolverError> {
-		use serde_yaml::Value::{Bool, Mapping, Null, Number, Sequence, String};
-		if let Null = mergee {
-			return Ok(self);
-		}
-		match self {
-			Null => {
-				*self = mergee;
-			}
-			Bool(_) => {
-				if !mergee.is_bool() {
-					return merge_compat_err! {self, mergee};
-				}
-				*self = mergee;
-			}
-			Number(_) => {
-				if !mergee.is_number() {
-					return merge_compat_err! {self, mergee};
-				}
-				*self = mergee;
-			}
-			String(_) => {
-				if !mergee.is_string() {
-					return merge_compat_err! {self, mergee};
-				}
-				*self = mergee;
-			}
-			Sequence(list) => {
-				if let Sequence(ref mut appendee) = mergee {
-					list.append(appendee);
-				} else {
-					return merge_compat_err! {self, mergee};
-				}
-			}
-			Mapping(mapping) => {
-				if let Mapping(superimposee) = mergee {
-					for (key, src) in superimposee {
-						if let Some(dst) = mapping.get_mut(&key) {
-							dst.merge(src)?;
-						} else {
-							mapping.insert(key, src);
-						}
-					}
-				} else {
-					return merge_compat_err! {self, mergee};
-				}
-			}
-		};
-		Ok(self)
-	}
-	fn merge_at(&mut self, key: &str, mergee: Self) -> Result<&mut Self, DataResolverError> {
-		if let Self::Mapping(mapping) = self {
-			let key: Self = key.into();
-			match mapping.get_mut(&key) {
-				Some(value) => {
-					value.merge(mergee)?;
-				}
-				None => {
-					mapping.insert(key, mergee);
-				}
-			}
-			Ok(self)
-		} else {
-			Err(DataResolverError::CannotMergeIntoNonMapping(self.clone()))
 		}
 	}
 }
--- src/data_resolver/values.rs
+++ src/data_resolver/values.rs
@@ -39,6 +39,89 @@ pub fn value_from_file(path: &PathBuf) -> Result<Value, DataResolverError> {
     Ok(value)
 }
 
+pub trait Merge {
+    fn merge(&mut self, mergee: Self) -> Result<&mut Self, DataResolverError>;
+    fn merge_at(&mut self, key: &str, mergee: Self) -> Result<&mut Self, DataResolverError>;
+}
+
+macro_rules! merge_compat_err {
+    ($self:expr, $mergee:expr) => {
+        Err(DataResolverError::IncompatibleYamlMerge {
+            dst: $self.clone(),
+            src: $mergee,
+        })
+    };
+}
+
+impl Merge for serde_yaml::Value {
+    fn merge(&mut self, mut mergee: Self) -> Result<&mut Self, DataResolverError> {
+        use serde_yaml::Value::{Bool, Mapping, Null, Number, Sequence, String};
+        if let Null = mergee {
+            return Ok(self);
+        }
+        match self {
+            Null => {
+                *self = mergee;
+            }
+            Bool(_) => {
+                if !mergee.is_bool() {
+                    return merge_compat_err! {self, mergee};
+                }
+                *self = mergee;
+            }
+            Number(_) => {
+                if !mergee.is_number() {
+                    return merge_compat_err! {self, mergee};
+                }
+                *self = mergee;
+            }
+            String(_) => {
+                if !mergee.is_string() {
+                    return merge_compat_err! {self, mergee};
+                }
+                *self = mergee;
+            }
+            Sequence(list) => {
+                if let Sequence(ref mut appendee) = mergee {
+                    list.append(appendee);
+                } else {
+                    return merge_compat_err! {self, mergee};
+                }
+            }
+            Mapping(mapping) => {
+                if let Mapping(superimposee) = mergee {
+                    for (key, src) in superimposee {
+                        if let Some(dst) = mapping.get_mut(&key) {
+                            dst.merge(src)?;
+                        } else {
+                            mapping.insert(key, src);
+                        }
+                    }
+                } else {
+                    return merge_compat_err! {self, mergee};
+                }
+            }
+        };
+        Ok(self)
+    }
+    fn merge_at(&mut self, key: &str, mergee: Self) -> Result<&mut Self, DataResolverError> {
+        if let Self::Mapping(mapping) = self {
+            let key: Self = key.into();
+            match mapping.get_mut(&key) {
+                Some(value) => {
+                    value.merge(mergee)?;
+                }
+                None => {
+                    mapping.insert(key, mergee);
+                }
+            }
+            Ok(self)
+        } else {
+            Err(DataResolverError::CannotMergeIntoNonMapping(self.clone()))
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
