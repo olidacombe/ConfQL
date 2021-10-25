ea6315325832b6c8ba7d16a7cc1d9b7a433d8e44
Oli Dacombe <odacombe@blockchain.com>
2021-10-25 22:16:25 +0100

    feat(array-filenames): some sorting for test determinism

--- proc-macro/tests/file_name_as_array_field.rs
+++ proc-macro/tests/file_name_as_array_field.rs
@@ -1,7 +1,7 @@
 use color_eyre::Result;
 use confql::graphql_schema;
 use indoc::indoc;
-use juniper::{graphql_value, EmptyMutation, EmptySubscription};
+use juniper::{graphql_value, EmptyMutation, EmptySubscription, Value};
 use test_files::TestFiles;
 
 graphql_schema! {
@@ -40,7 +40,7 @@ fn main() -> Result<()> {
     let ctx = Ctx::from(mocks.path().to_path_buf());
 
     // Run the executor.
-    let (res, _errors) = juniper::execute_sync(
+    let (mut res, _errors) = juniper::execute_sync(
         indoc! {"
             {
                 things {
@@ -55,13 +55,37 @@ fn main() -> Result<()> {
     )
     .unwrap();
 
+    let things = res
+        .as_mut_object_value()
+        .unwrap()
+        .get_mut_field_value("things")
+        .unwrap();
+    let mut new_things = things.as_list_value().unwrap().clone();
+    new_things.sort_by(|a, b| {
+        a.as_object_value()
+            .unwrap()
+            .get_field_value("name")
+            .unwrap()
+            .as_string_value()
+            .unwrap()
+            .cmp(
+                b.as_object_value()
+                    .unwrap()
+                    .get_field_value("name")
+                    .unwrap()
+                    .as_string_value()
+                    .unwrap(),
+            )
+    });
+    *things = Value::list(new_things);
+
     // Ensure the value matches.
     assert_eq!(
         res,
         graphql_value!({
             "things": [
-                {"name": "widget", "size": 1.1},
-                {"name": "dongle", "size": 2.2}
+                {"name": "dongle", "size": 2.2},
+                {"name": "widget", "size": 1.1}
             ]
         })
     );
